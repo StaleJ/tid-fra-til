42a5b2075e392c52136f9bf01194bdfd021463ba
Ståle <stalej@acos.no>
2023-10-31 08:34:57 +0100

    var to const

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function timeDifference(time1, time2) {
     // Convert both times to milliseconds
-    var time1_ms = new Date("1970-01-01T" + time1 + "Z").getTime();
-    var time2_ms = new Date("1970-01-01T" + time2 + "Z").getTime();
+    const time1_ms = new Date("1970-01-01T" + time1 + "Z").getTime();
+    const time2_ms = new Date("1970-01-01T" + time2 + "Z").getTime();
 
     // Calculate the difference in milliseconds
-    var difference_ms = Math.abs(time1_ms - time2_ms);
+    const difference_ms = Math.abs(time1_ms - time2_ms);
 
     // Convert back to hours, minutes, and seconds
-    var hours = Math.floor(difference_ms / 3600000);
-    var minutes = Math.floor((difference_ms % 3600000) / 60000);
-    var seconds = Math.floor((difference_ms % 60000) / 1000);
+    const hours = Math.floor(difference_ms / 3600000);
+    const minutes = Math.floor((difference_ms % 3600000) / 60000);
+    const seconds = Math.floor((difference_ms % 60000) / 1000);
 
     // Format the result as "hh:mm:ss"
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
@@ -20,10 +20,10 @@ document.getElementById("time-button").addEventListener("click", function () {
 
     const fromTime = document.getElementById("from").value;
     const toTime = document.getElementById("to").value;
-    let difference = timeDifference(fromTime, toTime);
+    const difference = timeDifference(fromTime, toTime);
 
 
-    let resultDiv = document.getElementById("result");
+    const resultDiv = document.getElementById("result");
     resultDiv.textContent = "Time difference: " + difference;
 
 
